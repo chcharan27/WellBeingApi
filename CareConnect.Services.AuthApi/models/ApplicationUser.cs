080b23f1f8045867716ca341071e48ac637ac088
chcharan27 <ch.charan27@gmail.com>
2025-04-30 17:27:17 +0530

    edied

--- CareConnect.Services.AuthApi/models/ApplicationUser.cs
+++ CareConnect.Services.AuthApi/models/ApplicationUser.cs
@@ -5,12 +5,6 @@ namespace CareConnect.Services.AuthApi.models
     public class ApplicationUser : IdentityUser
     {
         public string? Name { get; set; }
-
-        //public string? Email { get; set; }
-        //public string? Name { get; set; }
-        //public string? PhoneNumber { get; set; }
-        //public string? Password { get; set; }
-
         public int? Age { get; set; }
         public string? BloodGroup { get; set; }
         public string? MaritalStatus { get; set; }
